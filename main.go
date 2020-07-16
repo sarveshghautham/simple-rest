6e5740a392545c361d9b91372b832fd3e60b7b83
sarghau <sarvesh.kaladhar@gmail.com>
2020-07-15 22:14:23 -0700

    adding IDs to resp

--- main.go
+++ main.go
@@ -6,19 +6,27 @@ import (
 	"net/http"
 )
 
+// Entertainment is the struct to hold tv shows
+type Entertainment struct {
+	TVShows []TVShow `json:"tvShows"`
+}
+
 // TVShow struct holds the name and genre
 type TVShow struct {
+	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Genre string `json:"genre"`
 }
 
 func getMyShows(w http.ResponseWriter, r *http.Request) {
-	tvShows := []TVShow{
-		{Name: "Friends", Genre: "Comedy"},
-		{Name: "The Man in the High Castle", Genre: "Mystery"},
+	entertainment := Entertainment{
+		TVShows: []TVShow{
+			{ID: 1, Name: "Friends", Genre: "Comedy"},
+			{ID: 2, Name: "The Man in the High Castle", Genre: "Mystery"},
+		},
 	}
 
-	json.NewEncoder(w).Encode(tvShows)
+	json.NewEncoder(w).Encode(entertainment)
 }
 
 func handleRequests() {
